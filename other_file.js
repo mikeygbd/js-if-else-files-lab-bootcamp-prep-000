9e43b330718c01e4453578aa6e28ccc7974118de
Michael Soares <mikey.soares@gmail.com>
2018-10-16 05:35:15 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,6 +1,5 @@
 console.log("name")
-var index = require("./index.js")
-console.log(index.name)
 console.log("height")
 var index = require("./index.js")
+console.log(index.name)
 console.log(index.height)
